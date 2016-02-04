48b2a0b8b6b376f58716d0adaf1d010568b23c39
Jamie Stephens <james_stephens@cable.comcast.com>
2016-02-04 10:16:20 -0600

    Carve out wind() and temp() functions
    Used in 'examples/actions.sh' and in various demos.

--- examples/libs/wind.js
+++ examples/libs/wind.js
@@ -1,7 +1,15 @@
-function wind(where,appid) {
+function weather(where,appid) {
     var url = "http://api.openweathermap.org/data/2.5/weather?appid=" + appid + "&units=metric"
     var body = Env.http('GET', url + "&q=" + Env.encode(where));
     var weather = JSON.parse(body);
-    console.log('wind ' + weather.wind.speed);
-    return weather.wind.speed;
+    console.log('weather ' + weather);
+    return weather
+}
+
+function wind(where,appid) {
+    return weather(where, appid).wind.speed
+}
+
+function temp(where,appid) {
+    return weather(where, appid).main.temp
 }
