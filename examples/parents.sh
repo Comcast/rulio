0c77c65c388d31870f98cfd47c2c0b95d48e857a
Jamie Stephens <james_stephens@cable.comcast.com>
2016-07-07 16:13:05 -0500

    Fix strange typo re CHILD in example

--- examples/parents.sh
+++ examples/parents.sh
@@ -8,7 +8,7 @@ CHILD=bart
 PARENT="$CHILD.mom"
 
 # Clear the locations we'll use.
-curl -s "$ENDPOINT/api/loc/admin/clear?location=&CHILD"
+curl -s "$ENDPOINT/api/loc/admin/clear?location=$CHILD"
 curl -s "$ENDPOINT/api/loc/admin/clear?location=$PARENT"
 
 # Add a rule to the parent.
@@ -21,31 +21,31 @@ EOF
 
 # Set the child's parent.
 curl -s --data-urlencode 'set=["'"$PARENT"'"]' \
-    "$ENDPOINT/api/loc/parents?location=&CHILD"
+    "$ENDPOINT/api/loc/parents?location=$CHILD"
 
 # Use the 'parents' API to get the child's parent.
-curl -s "$ENDPOINT/api/loc/parents?location=&CHILD"
+curl -s "$ENDPOINT/api/loc/parents?location=$CHILD"
 
 # Send an event to the child and hope the parent's rule is triggered.
 curl -s --data-urlencode 'event={"wants":"chips"}' \
-   "$ENDPOINT/api/loc/events/ingest?location=&CHILD" | \
+   "$ENDPOINT/api/loc/events/ingest?location=$CHILD" | \
     python -mjson.tool
 
 # Disable the parent's rule in the child. 
-curl -s "$ENDPOINT/api/loc/rules/disable?location=&CHILD&id=momRule1"
+curl -s "$ENDPOINT/api/loc/rules/disable?location=$CHILD&id=momRule1"
 
 # Send another event and hope that the parent's rule, which is
 # disabled in the child, isn't triggered.
 curl -s --data-urlencode 'event={"wants":"chips"}' \
-   "$ENDPOINT/api/loc/events/ingest?location=&CHILD" | \
+   "$ENDPOINT/api/loc/events/ingest?location=$CHILD" | \
     python -mjson.tool
 
 # Enable the parent's rule in the child. 
-curl -s "$ENDPOINT/api/loc/rules/enable?location=&CHILD&id=momRule1"
+curl -s "$ENDPOINT/api/loc/rules/enable?location=$CHILD&id=momRule1"
 
 # Send an event to the child and hope the parent's rule is triggered.
 curl -s --data-urlencode 'event={"wants":"chips"}' \
-   "$ENDPOINT/api/loc/events/ingest?location=&CHILD" | \
+   "$ENDPOINT/api/loc/events/ingest?location=$CHILD" | \
     python -mjson.tool
 
 
@@ -63,7 +63,7 @@ EOF
 # Send an event to the child and hope the parent's rule isn't
 # triggered because the condition fails.
 curl -s --data-urlencode 'event={"demands":"chips"}' \
-   "$ENDPOINT/api/loc/events/ingest?location=&CHILD" | \
+   "$ENDPOINT/api/loc/events/ingest?location=$CHILD" | \
     python -mjson.tool
 
 # Write a parent fact.
@@ -74,19 +74,19 @@ curl -s -d 'pattern={"have":"?x"}' "$ENDPOINT/api/loc/facts/search?location=$PAR
   python -mjson.tool
 
 # Query the child for the parent fact.
-curl -s -d 'pattern={"have":"?x"}' "$ENDPOINT/api/loc/facts/search?location=&CHILD" | \
+curl -s -d 'pattern={"have":"?x"}' "$ENDPOINT/api/loc/facts/search?location=$CHILD" | \
   python -mjson.tool
 # Oops!  Not there.
 
 # Query the child for the parent fact (and say we want to find
 # inherited facts).
-curl -s -d 'pattern={"have":"?x"}' "$ENDPOINT/api/loc/facts/search?location=&CHILD&inherited=true" | \
+curl -s -d 'pattern={"have":"?x"}' "$ENDPOINT/api/loc/facts/search?location=$CHILD&inherited=true" | \
   python -mjson.tool
 # Fact found.  Better.
 
 # Send an event to the child and hope the parent's rule is triggered.
 curl -s --data-urlencode 'event={"demands":"chips"}' \
-   "$ENDPOINT/api/loc/events/ingest?location=&CHILD" | \
+   "$ENDPOINT/api/loc/events/ingest?location=$CHILD" | \
     python -mjson.tool
 
 
@@ -97,5 +97,5 @@ curl -s -d 'fact={"have":"tacos"}' "$ENDPOINT/api/loc/facts/add?location=$PARENT
 
 # Send an event to the child and hope the parent's rule is triggered.
 curl -s --data-urlencode 'event={"demands":"tacos"}' \
-   "$ENDPOINT/api/loc/events/ingest?location=&CHILD" | \
+   "$ENDPOINT/api/loc/events/ingest?location=$CHILD" | \
     python -mjson.tool
