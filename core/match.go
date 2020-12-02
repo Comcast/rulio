a8b93b13cd55eea7eff24faa4f780b920ab6533d
David Blyth <davidcooperblyth@gmail.com>
2020-12-02 12:16:00 -0700

    feat(core): perform casting around matcher

--- core/match.go
+++ core/match.go
@@ -5,17 +5,12 @@ import "github.com/Comcast/sheens/match"
 type Bindings match.Bindings
 
 var (
-	matcher                = match.DefaultMatcher
-	AllowPropertyVariables = matcher.AllowPropertyVariables
+	Matcher                = match.DefaultMatcher
+	AllowPropertyVariables = Matcher.AllowPropertyVariables
 )
 
 func Match(ctx *Context, pattern, fact interface{}, bs Bindings) ([]Bindings, error) {
-	bssExt, err := matcher.Match(pattern, fact, match.Bindings(bs))
-	bss := make([]Bindings, len(bssExt))
-	for i := range bssExt {
-		bss[i] = Bindings(bssExt[i])
-	}
-	return bss, err
+	return castBss(Matcher.Match(castMap(pattern), castMap(fact), match.Bindings(bs)))
 }
 
 func Matches(ctx *Context, pattern, fact interface{}) ([]Bindings, error) {
@@ -23,5 +18,38 @@ func Matches(ctx *Context, pattern, fact interface{}) ([]Bindings, error) {
 }
 
 func IsVariable(s string) bool {
-	return matcher.IsVariable(s)
+	return Matcher.IsVariable(s)
+}
+
+func castBss(bssExt []match.Bindings, err error) ([]Bindings, error) {
+	bss := make([]Bindings, len(bssExt))
+	for i := range bssExt {
+		bss[i] = Bindings(bssExt[i])
+	}
+	return bss, err
+}
+
+func castMap(iface interface{}) interface{} {
+	switch v := iface.(type) {
+	case Map, map[string]interface{}:
+		m, ok := v.(map[string]interface{})
+		if !ok {
+			m = map[string]interface{}(v.(Map))
+		}
+		for k, v := range m {
+			m[k] = castMap(v)
+		}
+		iface = m
+	case []interface{}:
+		for i := range v {
+			v[i] = castMap(v[i])
+		}
+		iface = v
+	default:
+		if v, ok := ISlice(v); ok {
+			iface = v
+		}
+	}
+
+	return iface
 }
